d27d183ee12f36cb62da0d28beaca658e6fd7bf5
Ali Anwar <allochi@gmail.com>
2012-12-17 11:40:40 +0100

    Change the name of decks and add a script deck for drawing example

--- 05-decks-types.lua
+++ 05-decks-types.lua
@@ -19,6 +19,10 @@ local layer = Util:layer ( viewport , true )
 -- MOAISurfaceDeck2D – A remnant of Moai’s original physics system. Unused at this time; left in at this time for my own convenience.
 -- MOAITileDeck2D – Subdivides a texture into N x M tiles that may then be indexed. Suitable for tile maps and sprite sheets where every sprite is the same size.
 
+-- ******************
+-- There are more Decks check documentation
+-- ******************
+
 -- MOAIGfxDevice
 -- MOAIGfxQuad2D
 -- MOAIGfxQuadDeck2D
@@ -27,25 +31,38 @@ local layer = Util:layer ( viewport , true )
 -- MOAIGfxState
 
 -- Deck - MOAIGfxQuad2D
-local quad2d = MOAIGfxQuad2D.new()
-      quad2d:setTexture("images/paper_lakitu.png")
-      quad2d:setRect(-60,-60,60,60)
-local quad2d_prop = MOAIProp2D.new()
-      quad2d_prop:setDeck(quad2d)
-      quad2d_prop:setLoc ( -size.width/2+60, size.height/2-60)
-      layer:insertProp(quad2d_prop)
+local deck = MOAIGfxQuad2D.new()
+      deck:setTexture("images/kaw-logo.png")
+      deck:setRect(-64,-64,64,64)
+local deck_prop = MOAIProp2D.new()
+      deck_prop:setDeck(deck)
+      deck_prop:setLoc ( -size.width/2+128, size.height/2-64)
+      layer:insertProp(deck_prop)
 
 -- Tile Deck - MOAITileDeck2D
-local quad2d_tile = MOAITileDeck2D.new()
-      quad2d_tile:setTexture("images/char.png")
-      quad2d_tile:setSize(3,4)
-      quad2d_tile:setRect(-64,-64,64,64)
-local quad2d_tile_prop = MOAIProp2D.new()
-      quad2d_tile_prop:setDeck(quad2d_tile)
-      quad2d_tile_prop:setIndex(7)
-      quad2d_tile_prop:setLoc (-size.width/2+70*2, size.height/2-60)
-      quad2d_tile_prop:setScl(0.7)
-      layer:insertProp(quad2d_tile_prop)
+local tile = MOAITileDeck2D.new()
+      tile:setTexture("images/char.png")
+      tile:setSize(3,4)
+      tile:setRect(-64,-64,64,64)
+local tile_prop = MOAIProp2D.new()
+      tile_prop:setDeck(tile)
+      tile_prop:setIndex(7)
+      tile_prop:setLoc (-size.width/2+128*2, size.height/2-64)
+      tile_prop:setScl(0.7)
+      layer:insertProp(tile_prop)
+
+-- Script Deck - MOAIScriptDeck
+function onDraw( index, xOff, yOff, xFlip, yFlip )
+  MOAIDraw.drawCircle (0,0,64,20)
+end
+local scriptDeck = MOAIScriptDeck.new()
+      scriptDeck:setRect ( -64, -64, 64, 64 )
+      scriptDeck:setDrawCallback ( onDraw )
+local script_prop = MOAIProp2D.new()
+      script_prop:setDeck(scriptDeck)
+      script_prop:setLoc (-size.width/2+128*3, size.height/2-64)
+      -- script_prop:setScl(0.7)
+      layer:insertProp(script_prop)
 
 
 
